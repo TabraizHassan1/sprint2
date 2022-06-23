5186ba8372b37b3041d0921b387556b440244326
Muhammad Tabraiz Hassan <tabraiz2017@namal.edu.pk>
2022-06-23 18:36:40 +0500

    second sprint

--- Tabraiz/sprint1/resources/hw_lambda.py
+++ Tabraiz/sprint1/resources/hw_lambda.py
@@ -1,4 +1,47 @@
+ 
+from datetime import datetime
+import http
+import urllib3
+from urllib import response
+
+
+
+URL_TO_MONITOR = 'skipq.org'
+
 
 def lambda_handler(event, context):
     print("hello world")
-    return
+    # i want to contain my latency and availablity metrics for my webresource
+    values= dict()
+    availability = getAvailability()
+    latency= getLatency()
+    values.update({"availability": availability, "Latency": latency})
+
+    return values
+
+
+#describe the parameters that your function takes
+#mention the return value
+def getAvailability():
+    http = urllib3.PoolManager()
+    response = http.request("GET", URL_TO_MONITOR)
+    if response.status == 200:
+        return 1.0
+    else:
+        return 0.0
+
+
+
+
+
+#describe the parameters that your function takes
+#mention the return value
+def getLatency():
+     http = urllib3.PoolManager()
+     start = datetime.now()
+     response = http.request("GET", URL_TO_MONITOR)
+     end = datetime.now()
+     delta = end - start                        #take time difference
+     latencySec = round(delta.microseconds * 0.000001,6)
+     return latencySec
+
--- Tabraiz/sprint1/sprint1/sprint1_stack.py
+++ Tabraiz/sprint1/sprint1/sprint1_stack.py
@@ -1,9 +1,14 @@
+from cgitb import handler
 from multiprocessing import reduction
 from aws_cdk import (
     # Duration,
+    RemovalPolicy,
     Stack,
     # aws_sqs as sqs,
-    aws_lambda as lambda_
+    aws_lambda as lambda_,
+    aws_events as events_,
+    aws_events_targets as target_
+    
 )
 from constructs import Construct
 
@@ -14,15 +19,29 @@ class Sprint1Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-
+        # create my lambda function for deploying hw_lamda.py
+      
         hw_lambda = self.create_lambda("MyFirstLambda", "hw_lambda.lambda_handler","./resources")
+        hw_lambda.apply_removal_policy(RemovalPolicy.DESTROY)
         # The code that defines your stack goes here
 
+        #defining an event
+        #https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_events/README.html
+        schedule = events_.Schedule.cron(minute="1")
+        target = target.LambdaFunction(handler= hw_lambda)
+        rule = events_.Rule(self, "LambdaRuleEvent",
+            description = 'this my rule to generate auto events for my hw_lamda', 
+            schedule= schedule,           
+            targets=[target]
+           )
         # example resource
         # queue = sqs.Queue(
         #     self, "Sprint1Queue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+
+    #https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_lambda/Function.html    
     def create_lambda(self,id_,handler, path ):
         return lambda_.Function(self, id_,
     runtime=lambda_.Runtime.PYTHON_3_7,
